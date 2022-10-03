a604d5b5700be8c100b778f5b494b19b89dcb142
konstantinos katrioplas <kokatrio@gmail.com>
2022-10-03 15:25:03 +0300

    use custom Complex struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
-use num_complex::Complex;
 use std::convert::TryInto;
+use std::ops::Add;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use web_sys::console::log;
@@ -19,6 +19,39 @@ macro_rules! log {
 //#[cfg(feature = "wee_alloc")]
 //#[global_allocator]
 //static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f64,
+    im: f64,
+}
+
+impl Complex {
+    fn new(re: f64, im: f64) -> Complex {
+        Complex { re, im }
+    }
+
+    fn square(self) -> Complex {
+        Complex {
+            re: self.re * self.re - self.im * self.im,
+            im: 2.0 * self.re * self.im,
+        }
+    }
+
+    fn norm(&self) -> f64 {
+        self.re * self.re + self.im * self.im
+    }
+}
+
+impl Add<Complex> for Complex {
+    type Output = Complex;
+    fn add(self, other: Complex) -> Self::Output {
+        Self {
+            re: self.re + other.re,
+            im: self.im + other.im,
+        }
+    }
+}
 
 struct MandelbrotSet {
     cxmin: f64,
@@ -47,9 +80,13 @@ fn palette(c: u32) -> Pixel {
         blue: b as u8,
         alpha: 255,
     }
+    //vec![b as u8, g as u8, r as u8, 255]
+    //pixels.push(b as u8);
+    //pixels.push(g as u8);
+    //pixels.push(r as u8);
+    //pixels.push(255);
 }
 
-#[wasm_bindgen]
 pub struct Plane {
     width: u32,
     height: u32,
@@ -57,20 +94,10 @@ pub struct Plane {
     set: MandelbrotSet,
 }
 
-#[wasm_bindgen]
 impl Plane {
-    pub fn new() -> Plane {
-        let width = 1000;
-        let height = 1000;
-        let pixels = (0..height * width)
-            .map(|_| Pixel {
-                red: 0,
-                green: 0,
-                blue: 0,
-                alpha: 0,
-            })
-            .collect();
-
+    fn new() -> Plane {
+        let width: u32 = 1000;
+        let height: u32 = 1000;
         let set = MandelbrotSet {
             cxmin: -2.0,
             cxmax: 0.47,
@@ -78,6 +105,7 @@ impl Plane {
             cymax: 1.12,
             iterations: 1000,
         };
+        let pixels = Vec::with_capacity((width * height).try_into().unwrap());
         Plane {
             width,
             height,
@@ -86,17 +114,7 @@ impl Plane {
         }
     }
 
-    pub fn npixels(&self) -> u32 {
-        self.width * self.height
-    }
-
-    pub fn pixels_ptr(&self) -> *const Pixel {
-        self.pixels.as_ptr()
-    }
-
-    pub fn calculate_set(&mut self) {
-        let mut pixels: Vec<Pixel> =
-            Vec::with_capacity((self.width * self.height).try_into().unwrap());
+    fn calculate_set(&mut self) {
         let dy = (self.set.cymax - self.set.cymin) / self.height as f64;
         for i in 0..self.height {
             let cy = self.set.cymin + i as f64 * dy;
@@ -105,43 +123,50 @@ impl Plane {
                 let cx = self.set.cxmin + j as f64 * dx;
 
                 let c = Complex::new(cx, cy);
-                let iters = self.compute_iterations(&c);
+                let iters = self.compute_iterations(c);
                 let pixel = palette(iters);
-                pixels.push(pixel);
+                self.pixels.push(pixel);
             }
         }
-
-        self.pixels = pixels;
     }
 
-    fn compute_iterations(&self, c: &Complex<f64>) -> u32 {
+    fn compute_iterations(&self, c: Complex) -> u32 {
         let mut z = Complex::new(0.0, 0.0);
         for i in 0..self.set.iterations {
-            z = z * z + c;
+            z = z.square() + c;
             if z.norm() > 2.0 {
                 return i;
             }
         }
         self.set.iterations
     }
+
+    //fn nbytes(&self) -> usize {
+    //    // each pixel is 4 bytes, RBGA
+    //    (self.width * self.height * 4).try_into().unwrap()
+    //}
 }
 
 #[wasm_bindgen]
 pub fn draw(ctx: &CanvasRenderingContext2d) -> Result<(), JsValue> {
     let mut plane = Plane::new();
     plane.calculate_set();
-    let width = plane.width;
-    let height = plane.height;
-    let pixels = plane.pixels;
+    //let pixels = plane.pixels;
+
+    //let mut data_array: Vec<u8> = Vec::with_capacity(plane.nbytes());
     let mut data_array: Vec<u8> = Vec::new();
-    pixels.iter().for_each(|p| {
+    plane.pixels.iter().for_each(|p| {
         data_array.push(p.red);
         data_array.push(p.green);
         data_array.push(p.blue);
         data_array.push(p.alpha);
     });
 
-    let image = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&data_array), width, height)?;
+    let image = ImageData::new_with_u8_clamped_array_and_sh(
+        Clamped(&data_array),
+        plane.width,
+        plane.height,
+    )?;
 
     ctx.put_image_data(&image, 0.0, 0.0)
 }
